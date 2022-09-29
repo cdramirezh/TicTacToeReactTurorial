ce6495a9e00c668c96e9876fbea5f6bfa3aad198
higi <cdramirezh@unal.edu.co>
2022-09-29 12:09:37 -0500

    Completing the game: Declaring a winner

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
@@ -37,8 +38,11 @@ class Board extends React.Component {
       Detecting changes is easier using const
     */ 
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares})
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
   }
 
   renderSquare(i) {
@@ -60,7 +64,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
